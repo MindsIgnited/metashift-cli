da67c1e1283b1a747a2767486214aa9ac57599d2
Navid Mitchell <navid@mindsignited.com>
2016-12-09 07:36:39 -0700

    no message

--- src/main/resources/crash/commands/gdev.groovy
+++ src/main/resources/crash/commands/gdev.groovy
@@ -2,7 +2,7 @@ package crash.commands
 
 import org.springframework.core.io.Resource
 
-(scan.resources { 'file:../metashift-spawn/resources/*' } | { Resource r ->
+(scan.resources { 'file:../genesys-generator/src/main/resources/metashift/**' } | { Resource r ->
 
     if(r.file.isDirectory() && !r.file.isHidden()) {
         addCmdMnt r.file.canonicalPath
